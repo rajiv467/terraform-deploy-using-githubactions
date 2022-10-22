57e7a73337cb9d5fb6023ed7cd2140fbdd1e5d56
rajiv467 <80842893+rajiv467@users.noreply.github.com>
2022-10-22 15:17:08 +0530

    Update and rename main.tf to ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.project}-${var.environment}"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  capacity_providers = ["FARGATE", ]
+}
+resource "aws_instance" "Service_instances" {
+  # for_each               = { for vm in var.configuration : vm.count => vm }
+
+  ami           = var.ami
+  instance_type = var.inst_type
+  # subnet_id              = aws_subnet.private_subnet.id
+  subnet_id                = aws_subnet.public_subnet_j.id
+  # vpc_security_group_ids = ["${data.aws_security_group.sec_grp.id}"]
+  vpc_security_group_ids = [aws_security_group.Sandbox-sg.id]
+  key_name               = var.key_name
+  get_password_data      = "true"
+  //user_data              = file("${path.module}/${each.value.userdata}")
+  tags = {
+    # Name = "${var.environment}_${each.value.name}"
+    Name = "Service_instances"
+  }
+}
